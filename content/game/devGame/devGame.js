2b75418976e953923d6d161add98fef346390229
Julian <tipler.julian@gmail.com>
2023-12-03 19:29:40 -0600

    moved devGame outside helpers

--- content/game/devGame/devGame.js
+++ content/game/devGame/devGame.js
@@ -1,4 +1,4 @@
-import { createGameContainer } from "../../createGameContainer.js";
+import { createGameContainer } from "../createGameContainer.js";
 
 const devGame = () => {
   const video = document.querySelector(".video");
